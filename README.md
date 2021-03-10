9d4b53bc9063d225f47a7aa14e3f04bb877ae26b
Iago Baldani <iagobaldanialmeida@hotmail.com>
2021-03-10 17:00:38 -0300

    Commit Inicial

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # FormSimplesJS
+
+Material de estudo
+
+Este repositório contém um simples formulário com validação de inputs com JavaScript.
---
+++ script.js
@@ -0,0 +1,79 @@
+const qsl = (elemento) => document.querySelector(elemento);
+
+
+
+let Validator = {
+    handleSubmit:(event)=>{
+        event.preventDefault();
+        let send = true;
+
+        let inputs = form.querySelectorAll('input');
+
+        Validator.cleanErrors();
+        
+        for(let i =0; i<inputs.length; i++){
+            let input = inputs[i];
+            let check = Validator.checkInput(input);
+
+            if(check !== true){
+                send = false;
+                Validator.showError(input, check);
+            }
+        }
+
+        if(send){
+            form.submit();
+        }
+
+    },
+
+    checkInput: (input) => {
+        let rules = input.getAttribute('data-rules');
+        if(rules !== null){
+            rules = rules.split('|');
+            for(let k in rules){
+                let rDetails = rules[k].split('=');
+
+                switch(rDetails[0]){
+                    case 'required':
+                        if(input.value == ''){
+                            return `O campo ${input.name.toUpperCase()} não pode ser vazio`;
+                        }
+                    break;
+                    case 'min':
+                        if(input.value.length < rDetails[1]){
+                            return `Sua senha não pode ter menos que ${ rDetails[1]} caracteres`;
+                        }
+                    break;
+                }
+            }
+        }       
+        return true;
+    },
+
+    showError: (input,error) =>{
+        input.style.borderColor = '#FF0000';
+
+        let errorElement = document.createElement('div');
+        errorElement.classList.add('error');
+        errorElement.innerHTML = error;
+
+        input.parentElement.insertBefore(errorElement, input.ElementSibling);
+    },
+
+    cleanErrors: () => {
+        let inputs = form.querySelectorAll('input');
+        for(let i=0; i < inputs.length; i++){
+            inputs[i].style='';
+        }
+
+        let errorElements = document.querySelectorAll('.error');
+        for(let i=0; i < errorElements.length; i++){
+            errorElements[i].remove();
+        }
+    }
+};
+
+let form = qsl('.validator');
+
+form.addEventListener('submit', Validator.handleSubmit);
